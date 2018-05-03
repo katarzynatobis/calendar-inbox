c1f9400cd720e47efa555a0adc4dc14323cb5d02
katawes <katawes@gmail.com>
2018-05-03 22:24:28 +0200

    actual date JS  + conversation boxes hover animation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,81 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function (event) {
+
+    let buttonOpenInbox = document.getElementById("arrow-chat"); //button for mobbile inbox
+    let counterInbox = true; //open or close inbox
+    let chatSection = document.getElementById("chat-section"); //whole chat section
+    let chatConversation = document.getElementsByClassName("chat-conversation"); // all conversation boxes
+
     
-    let buttonOpenInbox = document.getElementById("arrow-chat");
-    let counterInbox = true;
-    const chatSection = document.getElementById("chat-section");
+// open and close inbox on mobile
     
-    buttonOpenInbox.addEventListener("click", function(){
-        if (counterInbox){
+    buttonOpenInbox.addEventListener("click", function () {
+        if (counterInbox) {
             buttonOpenInbox.innerHTML = "INBOX &#8743;";
             chatSection.style.left = "0px";
-            counterInbox = !counterInbox;
-        }
-        else{
+        } else {
             buttonOpenInbox.innerHTML = "INBOX &#8744;";
-            chatSection.style.left = "-404px";
-            counterInbox = !counterInbox;
+            chatSection.style.left = (chatSection.clientWidth * (-1)) + "px";
+        }
+        counterInbox = !counterInbox;
+    })
+
+    
+//    hover animation of conversation box high
+    
+    for (let i = 0; i < chatConversation.length; i++) {
+        chatConversation[i].addEventListener("mouseover", function () {
+            for (let j = 0; j < chatConversation.length; j++) {
+                chatConversation[j].style.height = ((chatConversation[j].parentElement.clientHeight - ((chatConversation.length - 1) * 8) - 8 - 20) / chatConversation.length) + "px";
+            }
+
+            this.style.height = ((this.parentElement.clientHeight - ((chatConversation.length - 1) * 8) - 8) / chatConversation.length) + 16 + "px";
+        })
+    }
+
+    chatConversation[0].parentElement.addEventListener("mouseout", function () {
+        for (let j = 0; j < chatConversation.length; j++) {
+            chatConversation[j].style.height = ((chatConversation[j].parentElement.clientHeight - ((chatConversation.length - 1) * 8) - 8) / chatConversation.length) + "px";;
         }
     })
+
+
+
+//    show actual time on calendar
+    
+    
+    const now = new Date();
+    let actualDate = document.createElement("span");
+    let date;
+    actualDate.classList.add("line");
+    if (4 > 5 || 20 >= 21 || 20 < 7) {
+        console.log("poza tabela");
+    } else {
+        const dateHour = document.querySelectorAll('[data-hour="' + 14 + '"]')
+        for (let i = 0; i < dateHour.length; i++) {
+            date = dateHour[i].querySelectorAll('[data-day="' + 2 + '"]');
+            actualDate.style.height = (15 / 60) * date[0].clientHeight + 'px';
+            date[0].appendChild(actualDate);
+
+        }
+    }
+
+    //live version
+    //    if (now.getDay() > 5 || now.getHours() >= 21 || now.getHours() < 7 ){
+    //        console.log("poza tabela");
+    //    }
+    //    else{
+    //        const dateHour = document.querySelectorAll('[data-hour="' + now.getHours() + '"]')
+    //        for (let i = 0; i < dateHour.length; i++){
+    //            date = dateHour[i].querySelectorAll('[data-day="' + now.getDay() + '"]');
+    //            actualDate.style.height = (15 / 60) * date[0].clientHeight + 'px';
+    //            date[0].appendChild(actualDate);
+    //            
+    //        }
+    //    }
+
+
+
+
 });
