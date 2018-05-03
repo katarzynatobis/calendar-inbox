b32dd6220a8e4d2ca466efbedb0b879583a8bacd
katawes <katawes@gmail.com>
2018-05-03 23:59:19 +0200

    max string length for important message fixed + actual date highlighted

--- js/main.js
+++ js/main.js
@@ -43,57 +43,71 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 
-    //    show actual time on calendar
+    //      show actual time on calendar
 
+    //      highlight the day
 
     const now = new Date();
     let actualDate = document.createElement("span");
     let date;
     actualDate.classList.add("line");
-    if (4 > 5 || 20 >= 21 || 20 < 7) {
+
+    if (now.getDay() < 6) {
+        console.log(now.getDay());
+        let today = document.querySelectorAll('[data-day="' + now.getDay() + '"]');
+        for (let i = 0; i < today.length; i++) {
+            today[i].classList.add('today');
+        }
+    }
+
+    //      add line with actual time
+
+    if (now.getDay() > 5 || now.getHours() >= 21 || now.getHours() < 7) {
         console.log("poza tabela");
     } else {
-        const dateHour = document.querySelectorAll('[data-hour="' + 14 + '"]')
+        let actualDate = document.createElement("span");
+        let date;
+        actualDate.classList.add("line");
+        const dateHour = document.querySelectorAll('[data-hour="' + now.getHours() + '"]')
         for (let i = 0; i < dateHour.length; i++) {
-            date = dateHour[i].querySelectorAll('[data-day="' + 2 + '"]');
+            date = dateHour[i].querySelectorAll('[data-day="' + now.getDay() + '"]');
             actualDate.style.height = (15 / 60) * date[0].clientHeight + 'px';
             date[0].appendChild(actualDate);
 
         }
     }
 
-    //      live version
-    
-    //    if (now.getDay() > 5 || now.getHours() >= 21 || now.getHours() < 7 ){
-    //        console.log("poza tabela");
-    //    }
-    //    else{
-    //        const dateHour = document.querySelectorAll('[data-hour="' + now.getHours() + '"]')
-    //        for (let i = 0; i < dateHour.length; i++){
-    //            date = dateHour[i].querySelectorAll('[data-day="' + now.getDay() + '"]');
-    //            actualDate.style.height = (15 / 60) * date[0].clientHeight + 'px';
-    //            date[0].appendChild(actualDate);
-    //            
-    //        }
-    //    }
 
-    
     //      max string length depending on box size in textboxes
-    
-    let textboxes = document.getElementsByClassName("textbox")
+
+    let textboxes = document.getElementsByClassName("textbox");
+    let textboxesImportant = document.getElementsByClassName("message-important");
     let height = textboxes[0].parentElement.parentElement.clientHeight;
     let width = textboxes[0].parentElement.parentElement.clientWidth;
     let maxCharacters = ((height - 50) / 20) * ((width - 15) / 10);
+    let maxCharactersImportant = ( width / 10);
 
-    for (let i = 0; i < chatConversation.length; i++) {
+    for (let i = 0; i < textboxes.length; i++) {
         if (textboxes[i].innerText.length > maxCharacters) {
             textboxes[i].innerText = textboxes[i].innerText.substr(0, maxCharacters) + " ...";
         }
     }
     
+    for (let i = 0; i < textboxesImportant.length; i++) {
+        if (textboxesImportant[i].innerText.length > maxCharactersImportant) {
+            textboxesImportant[i].innerText = textboxesImportant[i].innerText.substr(0, maxCharactersImportant) + " ...";
+        }
+    }
+    
+    
+
+
+    // checkbox circle position - JS bacause IE doesn't support transitions with calc
+
     let checkbox = document.getElementsByTagName("label")[0];
     let circle = document.getElementsByClassName("circle")[0];
     let ifChecked = false;
+
     checkbox.addEventListener("click", function () {
         if (ifChecked) {
             circle.style.left = this.clientWidth - 3 + "px";
